cb46c359b40f4426bde961fd074e1ef9fc47d9fc
mariano-9r-ay2526 <mariano.dl@obmontessori.edu.ph>
2026-02-13 13:46:54 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+    function getArea() {
+        let base1 = Number(document.getElementById('base1').value);
+        let height1 = Number(document.getElementById('height1').value);
+        let area1 = (base1 * height1) / 2;
+        document.getElementById('area1').value = area1;
+
+    }
+
